d1af6c0acc0cc446d261563b8d2ef2eab2b6a413
Believe__It <374344282@qq.com>
2013-06-01 20:53:00 +0800

    登录用户中心模块，但是没有本地存储

--- EzHerald/src/com/herald/ezherald/account/AccountActivity.java
+++ EzHerald/src/com/herald/ezherald/account/AccountActivity.java
@@ -1,6 +1,9 @@
 package com.herald.ezherald.account;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.herald.ezherald.R;
@@ -15,6 +18,15 @@ public class AccountActivity extends SherlockActivity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.account_activity_main);
+		
 	}
+	 public void sendMessage(View view) {
+	        // Do something in response to button
+	    	Intent intent = new Intent(this,IDCardAccountActivity.class);
+	    	//EditText editText = (EditText) findViewById(R.id.edit_message);
+	    	//String message = editText.getText().toString();
+	    	//intent.putExtra(EXTRA_MESSAGE, message);
+	    	startActivity(intent);
+	    }
 	
 }
