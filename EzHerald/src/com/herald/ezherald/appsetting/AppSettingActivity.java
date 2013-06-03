9eef49542abb8c4486274c523fd07a7b457304d1
Boris He <iorange@126.com>
2013-06-03 22:10:35 +0800

    修复右侧滑动菜单错误，增加应用程序设置Activity

---
+++ EzHerald/src/com/herald/ezherald/appsetting/AppSettingActivity.java
@@ -0,0 +1,16 @@
+package com.herald.ezherald.appsetting;
+
+import android.os.Bundle;
+import com.herald.ezherald.R;
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class AppSettingActivity extends SherlockActivity {
+	//应用程序设置的Activity
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.app_setting_activity_main);
+	}
+
+}
