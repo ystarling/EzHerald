7415915a2b8a0baa8f7c9e4110db07d03004391d
funtion <xieshufang76@gmail.com>
2013-06-30 21:31:04 +0800

    gpa模块添加了系统菜单

--- EzHerald/src/com/herald/ezherald/gpa/GPAActivity.java
+++ EzHerald/src/com/herald/ezherald/gpa/GPAActivity.java
@@ -2,71 +2,48 @@ package com.herald.ezherald.gpa;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 
-import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.app.ActionBar.Tab;
-import com.actionbarsherlock.app.SherlockFragmentActivity;
-import com.herald.ezherald.R;
+import com.actionbarsherlock.view.MenuItem;
+import com.herald.ezherald.BaseFrameActivity;
 
 
 
-public class GPAActivity extends SherlockFragmentActivity {
+public class GPAActivity extends BaseFrameActivity {
+	Fragment mContentFrag;
+
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
-	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
+		
+		mContentFrag = new GpaFragment();
+		super.SetBaseFrameActivity(mContentFrag);
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.gpa_activity_main);
-		ActionBar bar = getSupportActionBar();
-        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        ActionBar.Tab tab1 = bar.newTab();
-        ActionBar.Tab tab2 = bar.newTab();
-        ActionBar.Tab tab3 = bar.newTab();
-        tab1.setText("绩点政策");
-        tab2.setText("Fragment B");
-        tab3.setText("Fragment C");
-        tab1.setTabListener(new MyTabListener());
-        tab2.setTabListener(new MyTabListener());
-        tab3.setTabListener(new MyTabListener());
-        bar.addTab(tab1);
-        bar.addTab(tab2); 
-        bar.addTab(tab3); 
-	    }
-	    
-	    private class MyTabListener implements ActionBar.TabListener
-	    {
 
-			@Override
-			public void onTabSelected(Tab tab, FragmentTransaction ft) {
-				if(tab.getPosition()==0)
-				{
-					FragmentA frag = new FragmentA();
-					ft.replace(android.R.id.content, frag);
-				}
-				else
-				{
-					//FragmentB frag = new FragmentB();
-					//ft.replace(android.R.id.content, frag);
-				}
-			}
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.actionbarsherlock.app.SherlockActivity#onCreateOptionsMenu(com.actionbarsherlock.view.Menu)
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu) {
+		
+		return super.onCreateOptionsMenu(menu);
+	}
 
-			@Override
-			public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onTabReselected(Tab tab, FragmentTransaction ft) {
-				// TODO Auto-generated method stub
-				
-			}
-	    	
-	    }
+	/* (non-Javadoc)
+	 * @see com.actionbarsherlock.app.SherlockActivity#onOptionsItemSelected(com.actionbarsherlock.view.MenuItem)
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		/*
+		 * 上侧Title位置的按钮点击相应
+		 */
+		return super.onOptionsItemSelected(item);
+	}
+	
+	
 
 }
