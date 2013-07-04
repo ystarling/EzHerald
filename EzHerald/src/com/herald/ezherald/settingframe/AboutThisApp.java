d766b31975ad35459c1a897dae579d4287b1e06f
Boris He <iorange@126.com>
2013-07-04 10:04:04 +0800

    “关于”Activity，右侧侧滑菜单在选中其他项后重复Activity bug 解决

--- EzHerald/src/com/herald/ezherald/settingframe/AboutThisApp.java
+++ EzHerald/src/com/herald/ezherald/settingframe/AboutThisApp.java
@@ -1,17 +1,30 @@
 package com.herald.ezherald.settingframe;
 
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.MenuItem;
 import com.herald.ezherald.R;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class AboutThisApp extends Activity {
+public class AboutThisApp extends SherlockActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.setting_activity_about);
+		
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch(item.getItemId()){
+		case android.R.id.home:
+			finish();
+		}
+		return super.onMenuItemSelected(featureId, item);
+	}
+	
+	
 }
