89703f9b330e199a4a2eac997d2660633c5419eb
funtion <xieshufang76@gmail.com>
2013-08-14 19:38:35 +0800

    修改新生指南

---
+++ EzHerald/src/com/herald/ezherald/freshman/FreshmanContent.java
@@ -0,0 +1,20 @@
+package com.herald.ezherald.freshman;
+
+import android.os.Bundle;
+
+import com.herald.ezherald.BaseFrameActivity;
+
+public class FreshmanContent extends BaseFrameActivity {
+	private FreshmanContentFragment mContentFrag;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		
+		Bundle bundle = getIntent().getExtras();
+		int type = bundle.getInt("type");
+		mContentFrag = new FreshmanContentFragment();
+		mContentFrag.setType(type);
+		super.SetBaseFrameActivity(mContentFrag);
+		super.onCreate(savedInstanceState);
+	}
+	
+}
--- EzHerald/src/com/herald/ezherald/freshman/FreshmanFragment.java
+++ EzHerald/src/com/herald/ezherald/freshman/FreshmanFragment.java
@@ -1,22 +1,57 @@
 package com.herald.ezherald.freshman;
-
+  
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.webkit.WebView;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockFragment;
 import com.herald.ezherald.R;
 
 public class FreshmanFragment extends SherlockFragment {
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle saved){
-		return inflater.inflate(R.layout.freshman_main,group,false);
+	TextView tv_study,tv_life,tv_play,tv_faq;
+	public static final int STUDY=0,LIFE=1,PLAY=2,FAQ=3;
+	class Listener implements OnClickListener{
+		private int type;
+		public Listener(int type){
+			this.type = type;
+		}
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(getActivity(),FreshmanContent.class);
+			Bundle bundle = new Bundle();
+			bundle.putInt("type", type);
+			intent.putExtras(bundle);
+			startActivity(intent);
+		}
+		
 	}
 	
+	void initView(){
+		tv_study = (TextView) getActivity().findViewById(R.id.text_study);
+		tv_life  = (TextView) getActivity().findViewById(R.id.text_life);
+		tv_play  = (TextView) getActivity().findViewById(R.id.text_play);
+		tv_faq   = (TextView) getActivity().findViewById(R.id.text_faq);
+		
+	}
+	void setOnclick(){
+		tv_study.setOnClickListener(new Listener(STUDY));
+		tv_life.setOnClickListener(new Listener(LIFE));
+		tv_play.setOnClickListener(new Listener(PLAY));
+		tv_faq.setOnClickListener(new Listener(FAQ));
+	}
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState){
 		super.onActivityCreated(savedInstanceState);
+		initView();
+		setOnclick();
+	}
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle saved){
+		//super.onCreate(savedInstanceState);
+			return inflater.inflate(R.layout.freshman_main,group,false);
 	}
 }
