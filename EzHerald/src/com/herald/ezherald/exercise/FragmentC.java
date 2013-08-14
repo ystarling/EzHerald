8a7599c48fcc22141dd738a1aabd0d2c3ea764c6
funtion <xieshufang76@gmail.com>
2013-08-14 11:39:14 +0800

    跑操加入账户

--- EzHerald/src/com/herald/ezherald/exercise/FragmentC.java
+++ EzHerald/src/com/herald/ezherald/exercise/FragmentC.java
@@ -1,5 +1,6 @@
 package com.herald.ezherald.exercise;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,85 +12,104 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.herald.ezherald.R;
+import com.herald.ezherald.account.Authenticate;
+import com.herald.ezherald.account.IDCardAccountActivity;
+import com.herald.ezherald.account.TyxAccountActivity;
+import com.herald.ezherald.account.UserAccount;
 
 public class FragmentC extends Fragment {
-	private TextView txt_rate,txt_remainDays,txt_notice,txt_updateTime;
+	private TextView txt_rate, txt_remainDays, txt_notice, txt_updateTime;
 	private Button btn_update;
 	private RunTimes runTimes;
-	public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle saved){
-		return inflater.inflate(R.layout.exercise_frag_c, group,false);
-	}
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState){
-		super.onActivityCreated(savedInstanceState);
-		
-		txt_rate = (TextView) getActivity().findViewById(R.id.txt_rate);
-		txt_remainDays = (TextView)getActivity().findViewById(R.id.txt_remain_days);
-		txt_notice = (TextView)getActivity().findViewById(R.id.txt_notice);
-		txt_updateTime = (TextView)getActivity().findViewById(R.id.txt_update_time);
-		btn_update = (Button)getActivity().findViewById(R.id.btn_update);
-		runTimes = new RunTimes(getActivity());
-		btn_update.setOnClickListener(new OnClickListener(){
+	private UserAccount user;
 
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
+	public View onCreateView(LayoutInflater inflater, ViewGroup group,
+			Bundle saved) {
+		return inflater.inflate(R.layout.exercise_frag_c, group, false);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		user = Authenticate.getTyxUser(getActivity());
+		if (null == user) {
+			Intent login = new Intent();
+			login.setClass(getActivity(), TyxAccountActivity.class);
+			startActivity(login);
+			getActivity().finish();
+		} else {
+			txt_rate = (TextView) getActivity().findViewById(R.id.txt_rate);
+			txt_remainDays = (TextView) getActivity().findViewById(
+					R.id.txt_remain_days);
+			txt_notice = (TextView) getActivity().findViewById(R.id.txt_notice);
+			txt_updateTime = (TextView) getActivity().findViewById(
+					R.id.txt_update_time);
+			btn_update = (Button) getActivity().findViewById(R.id.btn_update);
+			runTimes = new RunTimes(getActivity());
+			btn_update.setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View arg0) {
+					// TODO Auto-generated method stub
+					update();
+					show();
+				}
+
+			});
+			if (runTimes.isSet()) {
+				show();
+			} else {
 				update();
 				show();
 			}
-			
-		});
-		if(runTimes.isSet()){
-			show();
-		}else{
-			update();
-			show();
 		}
+
 	}
-	private void show(){
+
+	private void show() {
 		final String noData = "还没有数据";
-		
+
 		if (runTimes.getRate() != RunTimes.DEFAULT_RATE) {
 			txt_rate.setText(String.format("你的次数击败了\n%d%%的同学",
-					(int) (runTimes.getRate() * 100)));//TODO 1-runTimes.getRate()?
-		}else{
+					(int) (runTimes.getRate() * 100)));// TODO
+														// 1-runTimes.getRate()?
+		} else {
 			txt_rate.setText(noData);
 		}
-		
-		if (runTimes.getRemainDays() >=0 && runTimes.getAdviceTime()!=RunTimes.DEFAULT_ADVICE_TIME) {
+
+		if (runTimes.getRemainDays() >= 0
+				&& runTimes.getAdviceTime() != RunTimes.DEFAULT_ADVICE_TIME) {
 			txt_remainDays.setText(String.format("本学期还剩%d天\n建议你每周至少跑%d次",
 					runTimes.getRemainDays(), runTimes.getAdviceTime()));
-			if(runTimes.getAdviceTime()<=0){
+			if (runTimes.getAdviceTime() <= 0) {
 				txt_remainDays.setText("已经跑够了，要挑战满勤么？");
-			}else if(runTimes.getAdviceTime()<3)
+			} else if (runTimes.getAdviceTime() < 3)
 				txt_remainDays.setTextColor(Color.GREEN);
-			else if(runTimes.getAdviceTime()<=4)
+			else if (runTimes.getAdviceTime() <= 4)
 				txt_remainDays.setTextColor(Color.BLUE);
-			else 
+			else
 				txt_remainDays.setTextColor(Color.RED);
-		}else if(runTimes.getRemainDays() < 0){
+		} else if (runTimes.getRemainDays() < 0) {
 			txt_remainDays.setText("学期已经结束啦");
-		}else{
+		} else {
 			txt_remainDays.setText(noData);
 		}
-		
-		
+
 		if (runTimes.getAverageRunTime() != RunTimes.DEFAULT_AVERAGE_RUN_TIME) {
 			txt_notice.setText("平均打卡时间：" + runTimes.getAverageRunTime());
-		}else{
+		} else {
 			txt_notice.setText(noData);
 		}
 		if (runTimes.getUpdateTime() != RunTimes.DEFAULT_UPDATE_TIME) {
 			txt_updateTime.setText("更新于" + runTimes.getUpdateTime());
-		}else{
+		} else {
 			txt_updateTime.setText("");
 		}
-		
+
 	}
-	
-	private void update(){
+
+	private void update() {
 		runTimes.update();
 	}
-	
-	
+
 }
--- EzHerald/src/com/herald/ezherald/gpa/FragmentC.java
+++ EzHerald/src/com/herald/ezherald/gpa/FragmentC.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,57 +15,72 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import com.herald.ezherald.R;
+import com.herald.ezherald.account.Authenticate;
+import com.herald.ezherald.account.TyxAccountActivity;
+import com.herald.ezherald.account.UserAccount;
 
 public class FragmentC extends Fragment {
 	private CircleChat circleChat;
+	private UserAccount user;
+
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle saved)
-	{
+	public View onCreateView(LayoutInflater inflater, ViewGroup group,
+			Bundle saved) {
 		return inflater.inflate(R.layout.gpa_frag_c, group, false);
 	}
-	
+
 	@Override
-	public void onActivityCreated (Bundle savedInstanceState)
-	{
+	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-//		new GpaInfo(getActivity());
-		circleChat = (CircleChat) getActivity().findViewById(R.id.circleChat);
-		circleChat.setOnTouchListener(new OnTouchListener(){
+		user = Authenticate.getTyxUser(getActivity());
+		if (null == user) {
+			Intent login = new Intent();
+			login.setClass(getActivity(), TyxAccountActivity.class);
+			startActivity(login);
+			getActivity().finish();
+		} else {
+			circleChat = (CircleChat) getActivity().findViewById(
+					R.id.circleChat);
+			circleChat.setOnTouchListener(new OnTouchListener() {
 
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				// TODO Auto-generated method stub
-				if(event.getAction() == MotionEvent.ACTION_UP){//弹起时触发，否则影响滑动 
-					ArrayList<Record> records = circleChat.onTouch(event.getX(),event.getY());
-					if(records == null){//第一次运行可能没有数据
-						return true;
+				@Override
+				public boolean onTouch(View v, MotionEvent event) {
+					// TODO Auto-generated method stub
+					if (event.getAction() == MotionEvent.ACTION_UP) {// 弹起时触发，否则影响滑动
+						ArrayList<Record> records = circleChat.onTouch(
+								event.getX(), event.getY());
+						if (records == null) {// 第一次运行可能没有数据
+							return true;
+						}
+						AlertDialog.Builder builderSingle = new AlertDialog.Builder(
+								getActivity());
+						builderSingle.setIcon(R.drawable.ic_launcher);
+						builderSingle.setTitle("详 细 成 绩");
+						final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+								getActivity(),
+								android.R.layout.simple_list_item_1);
+						for (Record r : records) {
+							arrayAdapter.add(r.getName() + "   " + r.getScore());
+						}
+						builderSingle.setNegativeButton("关闭",
+								new DialogInterface.OnClickListener() {
+
+									@Override
+									public void onClick(DialogInterface dialog,
+											int which) {
+										dialog.dismiss();
+									}
+								});
+
+						builderSingle.setAdapter(arrayAdapter, null);
+						builderSingle.show();
+
 					}
-					AlertDialog.Builder builderSingle = new AlertDialog.Builder(
-		                    getActivity());
-		            builderSingle.setIcon(R.drawable.ic_launcher);
-		            builderSingle.setTitle("详 细 成 绩");
-		            final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-		            		getActivity(),
-		                    android.R.layout.simple_list_item_1);		            
-		            for(Record r:records){
-		            	arrayAdapter.add(r.getName()+"   "+r.getScore());
-		            }
-		            builderSingle.setNegativeButton("关闭",
-		                    new DialogInterface.OnClickListener() {
-
-		                        @Override
-		                        public void onClick(DialogInterface dialog, int which) {
-		                            dialog.dismiss();
-		                        }
-		                    });
-
-		            builderSingle.setAdapter(arrayAdapter,null);
-		            builderSingle.show();
-				
+					return true;
 				}
-				return true;
-			}
-			
-		});
+
+			});
+		}
 	}
+
 }
