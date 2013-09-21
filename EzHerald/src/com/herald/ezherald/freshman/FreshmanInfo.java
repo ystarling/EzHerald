528088f7491447f9b8f4e27e18be45ad06efe47e
funtion <xieshufang76@gmail.com>
2013-09-21 22:47:50 +0800

    新生指南联网更新（部分)

---
+++ EzHerald/src/com/herald/ezherald/freshman/FreshmanInfo.java
@@ -0,0 +1,89 @@
+package com.herald.ezherald.freshman;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Handler;
+
+public class FreshmanInfo {
+	public static final int STUDY=0,LIFE=1,PLAY=2,FAQ=3;
+	private static final String SharedPreferenceName = "FreshMan";
+	private List<List<String>> titles;
+	private List<List<String>> content;
+	private Activity activity;
+	private SharedPreferences shared;
+	private Handler handler;
+	private final String url = "http://herald.seu.edu.cn/xyzn/index/info_for_android/";
+	private void readShared(int type){
+		String title = shared.getString(SharedPreferenceName+"title"+STUDY, null);
+		String content = shared.getString(SharedPreferenceName+"content"+STUDY, null);
+		if(title!=null){
+			String[] temp = title.split("\\|");
+			for(int j=0;j<temp.length;j++){
+				titles.get(type).add(temp[j]);
+			}
+		}
+		if(content!=null){
+			String[] temp = title.split("\\|");
+			for(int j=0;j<temp.length;j++){
+				titles.get(type).add(temp[j]);
+			}
+		}
+		
+	}
+	FreshmanInfo(Activity activity){
+		for(int i=0;i<4;i++){
+			titles.add(new ArrayList<String>());
+			content.add(new ArrayList<String>());
+		}
+		this.activity = activity;
+		shared = activity.getSharedPreferences(SharedPreferenceName,0);
+		for(int i=0;i<4;i++){
+			readShared(i);
+		}
+		handler = new Handler(){
+			
+		};
+		
+		
+	}
+	public List<List<String>> getContent() {
+		return content;
+	}
+	public void setContent(List<List<String>> content) {
+		this.content = content;
+	}
+	public void update(){
+
+		new Thread(){
+			@Override
+			public void run(){
+				HttpClient client = new DefaultHttpClient();
+				HttpGet get = new HttpGet(url);
+				try {
+					HttpResponse response = client.execute(get);
+					if(response.getStatusLine().getStatusCode() != 200){
+						throw new Exception("can't link to herald");
+					}
+					String Message = response.getEntity().toString();
+					
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}.start();
+	}
+	public void save(){
+		
+	}
+}
