8d74c3476d359ae7a7828402ce7e6bdb7c638c79
OHMYGODZHY <shenhaidazhangyu@163.com>
2013-10-15 16:04:58 +0800

    Signed-off-by: OHMYGODZHY <shenhaidazhangyu@163.com> 加入主界面信息提交，进度条调整

---
+++ EzHerald/src/com/herald/ezherald/library/LibraryContentGrabber.java
@@ -0,0 +1,130 @@
+package com.herald.ezherald.library;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.herald.ezherald.account.Authenticate;
+import com.herald.ezherald.account.UserAccount;
+import com.herald.ezherald.mainframe.MainContentGridItemObj;
+import com.herald.ezherald.mainframe.MainContentInfoGrabber;
+
+public class LibraryContentGrabber implements MainContentInfoGrabber{
+	
+	public String content1;
+	Context context2=null;
+	public LibraryContentGrabber(Context cn){
+		this.context2=cn;
+	}
+	@Override
+	public MainContentGridItemObj GrabInformationObject() {
+		// TODO Auto-generated method stub
+		return provide();
+	}
+
+	public MainContentGridItemObj provide()
+	{
+		
+		MainContentGridItemObj item = new MainContentGridItemObj();
+			LibraryMineRemandThread th=new LibraryMineRemandThread(context2);
+			th.start();
+			item.setContent1(content1);
+			//item.setContent2("不会有超期");
+		return item;
+	}
+	
+	public class LibraryMineRemandThread extends Thread{
+		private Context context;
+		private JSONArray jsonarray;
+		
+		public LibraryMineRemandThread(Context cn){
+			this.context=cn;
+		}
+		@Override
+		public void run() {
+			// TODO 
+			try{
+				HttpResponse response=null;
+				try{
+				DefaultHttpClient client=new DefaultHttpClient();
+				List<NameValuePair> list=new ArrayList<NameValuePair>();
+				
+				UserAccount LibrAccount = Authenticate.getLibUser(context);
+				NameValuePair pair1=new BasicNameValuePair("username",LibrAccount.getUsername());
+				list.add(pair1);
+				NameValuePair pair2=new BasicNameValuePair("password",LibrAccount.getPassword());
+				list.add(pair2);	
+				
+				//NameValuePair pair1=new BasicNameValuePair("username","71111229");
+				//list.add(pair1);
+				//NameValuePair pair2=new BasicNameValuePair("password","213113709");
+				//list.add(pair2);
+				
+				UrlEncodedFormEntity entity = new UrlEncodedFormEntity(list,"UTF-8");
+				
+				
+				// 设置网络超时参数
+				HttpParams httpParams = client.getParams();
+				HttpConnectionParams.setConnectionTimeout(httpParams, 3000);
+				HttpConnectionParams.setSoTimeout(httpParams, 5000);
+				
+				LibraryUrl url=new LibraryUrl();
+				
+				HttpPost post=new HttpPost(url.getLIBRARY_MINE_BOOKS());
+				post.setEntity(entity);
+				
+				response=client.execute(post);
+				
+				}catch(Exception ex){
+					Log.d("Networking",ex.getMessage());
+					if(!ex.getMessage().isEmpty()){
+						content1="网络异常";
+					}
+				}
+				InputStream isr=response.getEntity().getContent();
+				BufferedReader br=new BufferedReader(new InputStreamReader(isr,"UTF-8"));
+				
+				String line=null;
+				StringBuffer sb=new StringBuffer();
+				while((line=br.readLine())!=null){
+					sb.append(line);
+				}
+		       
+				jsonarray=new JSONArray(sb.toString());
+				if(jsonarray.isNull(0)){
+					content1="还没有借书";
+				}else{
+					content1="最近需要归还"+jsonarray.getJSONObject(0).getString("libr_remand_date").toString();
+				}
+				
+		        String[] BookName=new String[jsonarray.length()];
+		        for(int i=0;i<jsonarray.length();i++){
+				JSONObject json=jsonarray.getJSONObject(i);
+				BookName[i] = json.getString("title");
+			 	Log.d("书名:",""+BookName[i]);
+		        }
+		        
+			}catch(Exception ex){
+				Log.d("LibraryMineRemandThread:",ex.getMessage());
+			}
+			
+		}
+		
+	}
+}
