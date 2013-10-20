e7c322fa58578db2975c5e6a39266e508bdd85be
Boris He <iorange@126.com>
2013-10-20 22:35:25 +0800

    修正更新频率的bug..抱歉了学弟

--- EzHerald/src/com/herald/ezherald/mainframe/SecondMenuFragment.java
+++ EzHerald/src/com/herald/ezherald/mainframe/SecondMenuFragment.java
@@ -277,17 +277,22 @@ public class SecondMenuFragment extends ListFragment {
 			if(mViewDestroyed)
 				return;
 			
-			Bundle bundle = msg.getData();
-			String name = bundle.getString(BUNDLE_KEY_USERNAME);
-			if(name == null)
-			{
-				Log.e("SecondMenuFragment", "Message contains nothing");
-				return;
+			try {
+				Bundle bundle = msg.getData();
+				String name = bundle.getString(BUNDLE_KEY_USERNAME);
+				if(name == null)
+				{
+					Log.e("SecondMenuFragment", "Message contains nothing");
+					return;
+				}
+				mMenuItemsStr[0] = name;
+				mListItems = getListItems();
+				mListViewAdapter.setmListItems(mListItems);
+				mListViewAdapter.notifyDataSetChanged();
+			} catch (NullPointerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			mMenuItemsStr[0] = name;
-			mListItems = getListItems();
-			mListViewAdapter.setmListItems(mListItems);
-			mListViewAdapter.notifyDataSetChanged();
 		}
 		
 	}
