29697fe8b4219a18c7b7706f175df07d7105622e
Boris He <iorange@126.com>
2013-10-30 20:32:48 +0800

    恢复大写的文件（）

---
+++ EzHerald/src/com/herald/ezherald/library/LibraryFragmentTabSelect.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package com.herald.ezherald.library;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+
+/**
+ * @author BIG_SEA
+ *	Tab—°‘Ò
+ */
+class LibraryFragmentTabSelect implements ActionBar.TabListener{
+
+	/**
+	 * 
+	 */
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		Fragment libr_frg = null;
+		switch(tab.getPosition()){
+			case 0:
+				libr_frg=new LibraryFragment();
+				break;
+//			case 1:
+//				libr_frg=new LibraryFragmentNews();
+//				break;
+			case 1:
+				libr_frg=new LibraryFragmentMine();	
+				break;
+		}
+		
+		ft.replace(android.R.id.content, libr_frg);
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
