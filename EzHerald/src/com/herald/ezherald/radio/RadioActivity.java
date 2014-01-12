89c7745dd8039f21eac8e62b9517507281508f51
funtion <xieshufang76@gmail.com>
2014-01-12 15:37:02 +0800

    写了一点广播台

--- EzHerald/src/com/herald/ezherald/radio/RadioActivity.java
+++ EzHerald/src/com/herald/ezherald/radio/RadioActivity.java
@@ -1,17 +1,42 @@
 package com.herald.ezherald.radio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 
 import com.herald.ezherald.BaseFrameActivity;
+import com.herald.ezherald.account.Authenticate;
+import com.herald.ezherald.account.IDCardAccountActivity;
+import com.herald.ezherald.account.UserAccount;
+import com.herald.ezherald.gpa.FailFragment;
+import com.herald.ezherald.gpa.GpaDbModel;
 
 public class RadioActivity extends BaseFrameActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		SetBaseFrameActivity(new RadioFragment());
-		super.onCreate(savedInstanceState);
+		UserAccount user = Authenticate.getIDcardUser(this);
+		if(null == user) {
+			turnToLogin();
+		} else {
+			SetBaseFrameActivity(new RadioFragment());
+			super.onCreate(savedInstanceState);
+		}
+	}
+	@Override
+	protected void onResume() {
+		UserAccount user = Authenticate.getIDcardUser(this);
+		if(null == user) {
+			turnToLogin();
+		} else {
+			super.onResume();
+		}
+	}
+	private void turnToLogin() {
+		Intent login = new Intent();
+		login.putExtra("remoteCall", true);
+		login.setClass(this,IDCardAccountActivity.class);
+		startActivity(login);
 	}
 }
